0ecfb02d1eafb1bad2a54839837ad1b626b946f6
Tiago Nobrega <tiago.bnobrega@gmail.com>
2019-01-31 23:01:35 -0200

    Updates

---
+++ pages-manifest-78ba06170cc5510df035.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{137:function(p){p.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"},{componentChunkName:"component---src-pages-print-js",jsonName:"print-d27",path:"/print/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E",index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","print-d27":"611/path---print-d-27-91c-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
+//# sourceMappingURL=pages-manifest-78ba06170cc5510df035.js.map
